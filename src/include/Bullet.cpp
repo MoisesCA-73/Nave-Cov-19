dc2f9e289e17318e8e6f773375ecda0c6ed819d9
fabrizio <fmattos@unsa.edu.pe>
2021-07-16 22:50:10 -0500

    Added some atributes and functions to bullet class

--- src/include/Bullet.cpp
+++ src/include/Bullet.cpp
@@ -4,7 +4,13 @@ Bullet::Bullet()
 {
 
 }
-
+Bullet::Bullet(sf::Texture& texture,float dir_x,float dir_y,float movement_speed)
+{
+    this->shape.setTexture(texture);
+    this->direction.x=dir_x;
+    this->direction.y=dir_y;
+    this->movementSpeed=movement_speed;
+}
 Bullet::~Bullet()
 {
 
--- src/include/Bullet.h
+++ src/include/Bullet.h
@@ -17,16 +17,22 @@
 class Bullet
 {
 private:
+    sf::Sprite shape;
     sf::Vector2f size;
+    sf::Vector2f direction;
     int damage;
     float fireRate;
+    float movementSpeed;
 public:
     //Constructor
     Bullet();
 
+    Bullet(sf::Texture& texture,float dir_x,float dir_y,float movement_speed);
     //Destructor
     ~Bullet();
-
+    //Functions
+    void update();
+    void render(sf::RenderTarget* target);
 };
 
 #endif  //BULLET_H

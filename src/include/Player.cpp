32d40c886af2366d69a4a52445c8ae98b9b83c93
xforenur <84652181+xforenur@users.noreply.github.com>
2021-07-28 19:17:57 -0500

    Update Player.cpp

--- src/include/Player.cpp
+++ src/include/Player.cpp
@@ -1,104 +1,100 @@
 #include "Player.h"
-//Private functions
 
-void Player::initVariables()
-{
-    this->movementSpeed = 6.f;
+void Player::initVariables(){
+    this->movementSpeed = 5.f;
+    
+    this->attackCooldownMax = 10.f;
+    this->attackCooldown = this->attackCooldownMax;
+
+    this->hpMax = 100;
+    this->hp = this->hpMax;
 }
 
-void Player::initTexture()
-{
-    if(!this->playerTexture.loadFromFile("Textures/nave.png"))
-    {
-        std::cout << "ERROR::PLAYER::INITTEXTURE:: Failed to load texture!" << '\n' ;
+void Player::initTexture(){
+    //Load a texture from file
+    if(!this->playerTexture.loadFromFile("Textures/nave.png")){//direccion de archivo donde esta la imagen de la nave
+        std::cout<<"Error::Player::Inittexture::could not load texture file."<<std::endl;
     }
+
 }
 
-void Player::initShape()
-{
-    this->shape.setTexture(&this->playerTexture);
-    this->shape.setSize(sf::Vector2f(50.f, 50.f));
-    this->shape.setPosition(375.f, 500.f);
+void Player::initSprite(){
+    this->sprite.setTexture(this->playerTexture);
+
+    //Cambiar el tamaño
+    this->sprite.scale(0.2f, 0.2f);  
 }
 
-//Constructor
-Player::Player(float x, float y)
-{
+ Player:: Player(){
     this->initVariables();
     this->initTexture();
-    this->initShape();
+    this->initSprite();
 }
 
-//Destructor
-Player::~Player()
-{
-
-}
-
-//Funciones publicas
-void Player::updateInput()
-{
-    //Entrada del teclado
-    //Hacia la izquierda
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-    {
-        this->shape.move(-this->movementSpeed, 0.f);
-    }
-    //Hacia la derecha
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-    {
-        this->shape.move(this->movementSpeed, 0.f);  
-    }
-    //Hacia arriba
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-    {
-        this->shape.move(0.f, -this->movementSpeed);  
-    }
-    //Hacia abajo
-    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        this->shape.move(0.f, this->movementSpeed);  
-    }
-}
-
-void Player::updateWindowBoundsCollision(const sf::RenderTarget * target)
-{
-    //Limite izquierdo
-
-    if (this->shape.getGlobalBounds().left <= 0.f)
-    {
-        this->shape.setPosition(0.f, this->shape.getGlobalBounds().top);
-    }
-    //Limite derecho
-    else if(this->shape.getGlobalBounds().left + this->shape.getGlobalBounds().width >= target->getSize().x)
-    {
-        this->shape.setPosition(target->getSize().x - this->shape.getGlobalBounds().width, this->shape.getGlobalBounds().top);
-    }
-    //Limite superior
-    if (this->shape.getGlobalBounds().top <= 0.f)
-    {
-        this->shape.setPosition(this->shape.getGlobalBounds().left, 0.f);
-    }
-    //Limite inferior
-    else if(this->shape.getGlobalBounds().top + this->shape.getGlobalBounds().height >= target->getSize().y)
-    {
-        this->shape.setPosition(this->shape.getGlobalBounds().left, target->getSize().y - this->shape.getGlobalBounds().height);
-    }
+ Player::~Player(){
 
 }
 
-void Player::updateBullet()
-{
-    bullet.update(this->shape.getPosition());
+const sf::Vector2f & Player::getPos() const{
+    return this->sprite.getPosition();
 }
-void Player::update(const sf::RenderTarget* target)
-{
-    this->updateInput();
-    this->updateWindowBoundsCollision(target);
-    this->updateBullet();
+
+const sf::FloatRect Player::getBounds() const{
+    return this->sprite.getGlobalBounds();
 }
-void Player::render(sf::RenderTarget* target)
-{
-    target->draw(this->shape);
-    this->bullet.render(target);
+
+const int & Player::getHp() const{
+    return this->hp;
+}
+
+const int & Player::getHpMax() const{
+    return this->hpMax;
+}
+
+void Player::setPosition(const sf::Vector2f pos){
+    this->sprite.setPosition(pos);
+}
+
+void Player::setPosition(const float x, const float y){
+    this->sprite.setPosition(x,y);
+}
+
+void Player::setHp(const int hp){
+    this->hp= hp;
+}
+
+void Player::loseHp(const int value){
+    this->hp -=value;
+    if(this->hp < 0){
+        this->hp = 0 ;
+    }
+}
+
+//Functions
+void Player::move(const float dirX, const float dirY){
+    this->sprite.move(this->movementSpeed * dirX, this->movementSpeed * dirY);
+}
+
+const bool Player::canAttack(){
+    if(this->attackCooldown >= this->attackCooldownMax){
+        this->attackCooldown = 0.f;
+        return true;
+    }
+    return false;
+}
+
+void Player::updateAttack(){
+    if(this->attackCooldown < this->attackCooldownMax){
+        this->attackCooldown += 0.5f;
+    }
+}
+
+void Player::update(){
+    this->updateAttack();
+}
+
+//Representa nuestro sprite
+void Player::render(sf::RenderTarget& target){
+    target.draw(this->sprite);
+
 }
